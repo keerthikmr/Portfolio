ccec50e57bffd26b1c7c934e2fc6de878a891fe1
keerthikmr <keerthikumar.m10@gmail.com>
2024-05-22 13:13:14 +0530

    Add projects with icons

--- portfolio-app/src/app/projects/project-card/project-card.component.ts
+++ portfolio-app/src/app/projects/project-card/project-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button'; 
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule ],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.css'
 })
