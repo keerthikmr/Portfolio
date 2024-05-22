64360897f3ba17a0fd6d398ec4fa830fe18b1809
keerthikmr <keerthikumar.m10@gmail.com>
2024-05-22 15:51:48 +0530

    Add experience components

---
+++ portfolio-app/src/app/experience/experience-list/experience-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './experience-list.component.html',
+  styleUrl: './experience-list.component.css'
+})
+export class ExperienceListComponent {
+  experiences = [
+    { title: 'Intern', org: 'Enerpac India Pvt Limited', year: 'May 2024 - June 2024', work: ['Built Angular app', 'Did this', 'Did that'] },
+    { title: 'Web developer', org: 'Chingu', year: 'Jan 2024 - April 2024', work: ['Built React app', 'Used APIs', 'Built interactive maps'] },
+    { title: 'Researcher', org: 'MulticoreWare Inc', year: 'Aug 2022 - Present', work: ['Research on H.265 video codec', 'Built compression algorithms', 'Created partition, prediction, encode and decode mechanisms'] },
+  ]
+}
--- portfolio-app/src/app/experience/experience.component.ts
+++ portfolio-app/src/app/experience/experience.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ExperienceCardComponent } from './experience-card/experience-card.component';
+import { ExperienceListComponent } from './experience-list/experience-list.component';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [ExperienceCardComponent, ExperienceListComponent],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
