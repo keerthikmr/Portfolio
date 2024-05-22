a40643fb9971cf7033011de465081be981251f97
keerthikmr <keerthikumar.m10@gmail.com>
2024-05-22 17:52:36 +0530

    Style experience cards

--- portfolio-app/src/app/experience/experience-card/experience-card.component.ts
+++ portfolio-app/src/app/experience/experience-card/experience-card.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button'; 
 
 @Component({
   selector: 'app-experience-card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './experience-card.component.html',
   styleUrl: './experience-card.component.css'
 })
--- portfolio-app/src/app/experience/experience-list/experience-list.component.ts
+++ portfolio-app/src/app/experience/experience-list/experience-list.component.ts
@@ -9,8 +9,8 @@ import { Component } from '@angular/core';
 })
 export class ExperienceListComponent {
   experiences = [
-    { title: 'Intern', org: 'Enerpac India Pvt Limited', year: 'May 2024 - June 2024', work: ['Built Angular app', 'Did this', 'Did that'] },
-    { title: 'Web developer', org: 'Chingu', year: 'Jan 2024 - April 2024', work: ['Built React app', 'Used APIs', 'Built interactive maps'] },
-    { title: 'Researcher', org: 'MulticoreWare Inc', year: 'Aug 2022 - Present', work: ['Research on H.265 video codec', 'Built compression algorithms', 'Created partition, prediction, encode and decode mechanisms'] },
+    { title: 'Intern', org: 'Enerpac India Pvt Limited', year: 'May 2024 - June 2024', work: ['Built Angular app', 'Did this', 'Did that'], comp_badge_url: 'https://google.com' },
+    { title: 'Web developer', org: 'Chingu', year: 'Jan 2024 - April 2024', work: ['Built React app', 'Used APIs', 'Built interactive maps'], comp_badge_url: 'https://google.com' },
+    { title: 'Researcher', org: 'MulticoreWare Inc', year: 'Aug 2022 - Present', work: ['Research on H.265 video codec', 'Built compression algorithms', 'Created partition, prediction, encode and decode mechanisms'], comp_badge_url: 'https://google.com' },
   ]
 }
